5d47148f036b8fa40d428171f4bb837e9dfb6c80
Igor <igor3108almeida@gmail.com>
2025-03-27 21:55:48 -0300

    adicionando aula05

---
+++ Aulas/aula04/App.js
@@ -0,0 +1,8 @@
+import GaleriaFotos from "./screens/GaleriaFotos";
+import RegistrarConta from "./screens/RegistrarConta";
+import DetalhesProduto from "./screens/DetalhesProduto";
+function App() {
+  return <DetalhesProduto />
+}
+
+export default App;
